75314e5300467214ad09007b728942a66c7ea151
root <root@9771.com>
2026-07-24 21:39:24 +0000

    defite construct

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ class Car{
     public string $brand;
     public string $color;
 
-
     public function __construct( string $brand, string $color ){
         $this -> brand = $brand;
         $this -> color = $color;
@@ -17,9 +16,8 @@ class Car{
     }
 }
 
-$myCar = new Car();
-$myCar -> brand = "Tayota";
-$myCar -> color = "white";
+$myCar = new Car("Tayota", "Black");
+// $out = 
 
-echo $myCar->outMethod();
+echo $out; 
 echo "\n";
