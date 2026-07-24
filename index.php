ef9795e2b78905e10d33c85569fe06e372da06f3
root <root@9771.com>
2026-07-24 21:36:25 +0000

    add construct

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php
 
 
-class Car{
+class Car{  
+
     public string $brand;
     public string $color;
 
+
+    public function __construct( string $brand, string $color ){
+        $this -> brand = $brand;
+        $this -> color = $color;
+    }   
+    
     public function outMethod() : string {
         return "Машина " . $this -> brand . " с цветом " . $this -> color . " был здесь !" ; 
     }
-
 }
 
 $myCar = new Car();
-
 $myCar -> brand = "Tayota";
 $myCar -> color = "white";
 
