2e5bdb227becaa96ac79e6d37be7d2469d40e941
root <root@9771.com>
2026-07-24 20:47:21 +0000

    change name class

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
 
-class helloWorld{
-    public string $car;
+class car{
+    public string $brand;
     public string $color;
 
     public function nameColor() : string {
-        
+        return "Машина " . $this -> brand . " с цветом " . $this -> color . "был здесь !" ; 
     }
 
 }
