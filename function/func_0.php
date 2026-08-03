53a146f846f4ae6e49d0c7fe0bd900391172526d
root <root@9771.com>
2026-08-03 14:17:37 +0000

    анонимная функция

--- function/func_0.php
+++ function/func_0.php
@@ -1,5 +1,6 @@
 <?php
 
+declare(strict_types=1);
 // анонимный функция
 $name = "Иван";
 
@@ -9,5 +10,16 @@ $sayHello = function() use ($name, &$num) {
     return "Привет, " . $name . " ". $num . " раз. \n"; 
 };
 
-echo $sayHello();
+// echo $sayHello();
 
+// 1. хочу вызвать в функции, функцию
+// 2. хочу обявлять функции в перемеенный 
+
+function number(int $a) :int {
+    return  $a = $a*5;
+}
+$testVar = number(22);
+
+echo $testVar + $testVar;
+
+echo "\n";
